562eb8cdf228b0c307513839fa646f80e48bd86f
Jiles92 <legendnight@126.com>
2016-08-05 16:42:15 +0800

    swift 导航页制作

---
+++ WelcomePage/WelcomePage/SecondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.swift
+//  WelcomePage
+//
+//  Created by 洛风 on 15/8/4.
+//  Copyright (c) 2015年 洛风. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
